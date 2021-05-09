a4383424c8f94a55100cb48afb2ce73d9b7126a5
VEGLEZ <valentin.eduardo.g94@gmail.com>
2021-05-08 20:31:38 -0500

    Social Media Dashboard frontendmentor challenge finished

--- src/pages/frontendmentor/SocialMediaDashboardWithThemeSwitcherMaster.jsx
+++ src/pages/frontendmentor/SocialMediaDashboardWithThemeSwitcherMaster.jsx
@@ -1,28 +1,19 @@
 import React from 'react';
 import Styles from '../../styles/pages/frontendmentor/SMDWTSM.module.scss';
-import iconYoutube from '@frontendmentorAssets/SMDWTSM/icon-youtube.svg';
-import iconDown from '@frontendmentorAssets/SMDWTSM/icon-down.svg';
-import iconInstagram from '@frontendmentorAssets/SMDWTSM/icon-instagram.svg';
-import iconUp from '@frontendmentorAssets/SMDWTSM/icon-up.svg';
-import iconFacebook from '@frontendmentorAssets/SMDWTSM/icon-facebook.svg';
-import iconTwitter from '@frontendmentorAssets/SMDWTSM/icon-twitter.svg';
+
+import MyThemeProvider from '@frontendmentorComponents/SMDWTSM/theme/themeContext';
+import PageContainer from '../../components/frontendmentor/SMDWTSM/PageContainer/PageContainer';
 
 const SocialMediaDashboardWithThemeSwitcherMaster = () => {
   return (
-    <div className={Styles.container}>
-      <h1> Estas en la página SocialMediaDashboardWithThemeSwitcherMaster </h1>
-      <section>
-        Social Media Dashboard
-        <p>Total Followers: 23,004</p>
-        <p>Dark Mode</p>
-        <div>@nathanf 1987 Followers 12 Today</div>
-        <div>@nathanf 1044 Followers 99 Today</div>
-        <div>@realnathanf 11k Followers 1099 Today</div>
-        <div>Nathan F. 8239 Subscribers 144 Today</div>
-        <p>Overview - Today</p>
-        <div>Page Views 87 3</div>
-      </section>
-    </div>
+    <MyThemeProvider>
+      <PageContainer
+        className={{
+          container: Styles.container,
+          overviewCards: Styles.overviewCards,
+        }}
+      />
+    </MyThemeProvider>
   );
 };
 
