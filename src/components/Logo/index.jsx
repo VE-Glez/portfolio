3547f4edf6700a3e5fd103508657e350602553b4
VEGLEZ <valentin.eduardo.g94@gmail.com>
2021-08-11 15:56:13 -0500

    Converting the svg logo into a react component

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import styles from './styles.module.scss';
+
+function Brand(props) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 85.51 85.13'
+      {...props}
+      className={styles.brand}
+    >
+      <defs>
+        {/* <style>
+          {
+            '.prefix__cls-1{fill:none;stroke:#14152d;stroke-miterlimit:10;stroke-width:.75px}'
+          }
+        </style> */}
+      </defs>
+      <g id='prefix__Capa_2' data-name='Capa 2'>
+        <g id='prefix__Capa_1-2' data-name='Capa 1'>
+          <path
+            className='prefix__cls-1'
+            d='M9.72 79.95v4.8h3.67v-4.8h3.15v4.8h3.54v-4.8h3.79v4.8h3.28v-4.8h3.41v4.8h3.79v-4.8h3.16v4.8h3.66v-4.8h3.29v4.8h3.92v-4.8h3.15v4.8h3.79v-4.8h2.66v4.8h4.16v-4.8h2.91v4.8h4.29v-4.8h2.78v4.8h4.04v-4.8h2.91V76.8h6.06v-4.55h-5.81l-.25-2.91h6.06V65.3h-5.81l-.12-2.78h5.93v-4.04H79.2v-3.16h5.93v-4.17H79.2l-.13-2.65h6.06v-4.17H79.2V41.3h5.93v-4.04H79.2l.12-2.65h5.81v-4.17h-5.68v-3.03h5.68v-4.05h-5.81l-.12-2.9h5.93v-4.3h-5.81v-2.85h5.81V9.85H79.2V6.19h-3.04V.38h-4.04v6.06h-3.03V.38H64.8v6.06h-2.66V.38h-4.29v6.31h-2.53V.38h-4.29v6.06h-2.78V.38h-4.17v6.31h-3.03V.38h-4.17v6.18h-3.03V.38h-3.79v6.31h-2.91V.38h-4.04v6.06h-3.03V.38h-4.04v6.06h-2.91V.38H8.71v6.06H6.06v3.41H.38v4.04h5.81v3.03H.38v3.92h5.81v2.78H.38v3.91h5.93v2.91H.38v4.42h6.06v2.78H.38v3.79h6.18v2.9H.38v4.42h5.93v2.91H.38v4.04h6.06v2.65H.38v4.93h5.93v2.4H.38v3.92h6.31v3.03H.38v4.17h6.18v2.65l3.16.5z'
+          />
+          <path
+            className='prefix__cls-1'
+            d='M12.75 9.41h60.38v1.26h1.52v2.2h1.01v59.06h-.88v1.52h-1.02v1.39h-1.38l-.13.88H49.89l-.13-2.02H35.74v2.02H12.75v-1.26h-1.01v-1.01h-.88v-.76H9.85V12.87h1.01v-1.95h.88V9.41h1.01z'
+          />
+          <path
+            className='prefix__cls-1'
+            d='M20.33 41.44l14.15-7.29v2.11l-11.93 6v.06l11.93 6v2.1l-14.15-7.33zM36.46 51.6L45 29.49h2.08L38.51 51.6zm26.66-8.48L49 50.38v-2.1l12-6v-.06l-12-6v-2.07l14.15 7.26z'
+          />
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export default Brand;
