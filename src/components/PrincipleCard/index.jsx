e2e0540411879043791eceadf93348f90d9adf6c
VEGLEZ <valentin.eduardo.g94@gmail.com>
2021-08-14 00:46:24 -0500

    adding underline to h tag

--- src/components/PrincipleCard/index.jsx
+++ src/components/PrincipleCard/index.jsx
@@ -6,7 +6,9 @@ const PrincipleCard = (props) => {
 
   return (
     <article className={styles.container}>
-      {React.createElement(icon, { className: styles.icon })}
+      <span className={styles.icon__weaooer}>
+        {React.createElement(icon, { className: styles.icon })}
+      </span>
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
     </article>
